7745b70066050e888dc2a8ca5e19935f22152ea2
Víctor <lacuentadevictor@gmail.com>
2023-05-13 05:06:47 +0200

    Documented javascript-user-interface.js

--- domain/javascript-user-interface.js
+++ domain/javascript-user-interface.js
@@ -1,3 +1,11 @@
+/**
+    This is one of the multiple user interfaces that the application could have.
+    User interfaces translate user actions into operations over the system through the specific interface.
+    
+    The user wants to sum two numbers and we translate that into the instantiation and usage of the calculator.
+    An http-api user interface would translate the same action into an http call.
+**/
+
 const Calculator = require("./calculator")
 
 module.exports = function(){
